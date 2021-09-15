3b251a9b643699c49a8abb9d7fda82f0d967f7c8
anurag <anuragkumarjsk13@gmail.com>
2021-09-15 16:12:03 +0530

    15/09

---
+++ aucti-web/build/static/js/12.99929804.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[12],{222:function(t,c,e){"use strict";e.r(c);e(2);var r=e(1);c.default=function(t){return Object(r.jsx)(r.Fragment,{children:Object(r.jsx)("img",{src:t.src,className:"object-cover object-center h-40 pb-4",onLoad:t.onLoad,onError:t.onError,alt:t.alt})})}}}]);
+//# sourceMappingURL=12.99929804.chunk.js.map
